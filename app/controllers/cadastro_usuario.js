5441853e26b4d38e503588604ae55c1b82acbd26
firebird-t <llinharespinheiro@gmail.com>
2018-04-27 22:27:51 -0300

    callback hell

--- app/controllers/cadastro_usuario.js
+++ app/controllers/cadastro_usuario.js
@@ -1,14 +1,16 @@
-module.exports.cadastrar_usuario = function(app, request, response){
 
+module.exports.cadastrar_usuario = function (app, request, response) {
+
+	async = require('async');
 	var connection = app.config.dbconn();
 	var cadUser = new app.app.models.dados_usuariosDAO(connection);
-	
+
 	var body = request.body;
 	var cadastro = false;
 	var erro_cadastro = {};
-	
-	erro_cadastro[0] = {'msg':'usuário existente'};
-	erro_cadastro[1] = {'msg':'email existente, insira outro'};
+
+	erro_cadastro[0] = { 'msg': 'usuário existente' };
+	erro_cadastro[1] = { 'msg': 'email existente, insira outro' };
 
 	request.assert('email', 'O campo email não pode ficar vazio').trim().notEmpty().isEmail();
 	request.assert('nome_usuario', 'O campo nome de usuário não pode ficar vazio').trim().notEmpty();
@@ -18,44 +20,56 @@ module.exports.cadastrar_usuario = function(app, request, response){
 
 	var erros = request.validationErrors();
 
-	if(erros){
+	if (erros) {
 		response.send(erros);
 		return;
 	}
 
-	
-
-	cadUser.validaNomeUsuario(body.nome_usuario,function(error, result){
-		if(result.length > 0){
-			return;
-		}
-	});
-
 	//console.log(cadastro)
-	cadUser.validaEmail(body.email,function(error, result){
-		if(result.length > 0){			
-			return;
-		}
-	});
 
-	
-	cadUser.cadastrar(body, function(error, result){
-		if(error) {
-			response.send('Falha ao cadastrar o usuário:' + error);
+	async.series([
+		function (callback) {
+			cadUser.validaNomeUsuario(body.nome_usuario, function (error, result) {
+				if (result.length > 0) {
+					console.log('1');
+					callback('false', result)
+				}else{
+					callback(null, result)
+				}
+			});
+		},
+		function (callback) {
+			cadUser.validaEmail(body.email,function(error, result){
+				if (result.length > 0) {
+					console.log('2');
+					callback('false', result)
+				}else{
+					callback(null, result)
+				}
+			});
+		}, function (callback) {
+			cadUser.cadastrar(body, function(error, result){
+				if(error) {
+					response.send('Falha ao cadastrar o usuário:' + error);
+				}
+				else{
+					request.session.autorizado = true;
+					request.session.nome = body.nome;
+					request.session.user = body.nome_usuario;
+					request.session.user_id = result.insertId;
+					response.redirect('/home');
+				}
+			})
 		}
-		else{
-			request.session.autorizado = true;
-			request.session.nome = body.nome;
-			request.session.user = body.nome_usuario;
-			request.session.user_id = result.insertId;
-			response.redirect('/home');
+	],function(err, results){
+		if(err){
+			response.send('Houve algo de ruim na requisição seu fdp no nível:' + result);
 		}
 	})
-	
 
 }
 
-module.exports.recuperar_dados_cadastro = function(app, request, response){
+module.exports.recuperar_dados_cadastro = function (app, request, response) {
 	var connection = app.config.dbconn();
 	var cadUser = new app.app.models.dados_usuariosDAO(connection);
 
@@ -64,13 +78,13 @@ module.exports.recuperar_dados_cadastro = function(app, request, response){
 	// })
 }
 
-module.exports.checa_nome_usuario = function(app, request, response){
+module.exports.checa_nome_usuario = function (app, request, response) {
 
 	var v_name_user = new app.app.controllers.dados_usuariosDAO;
 	var body = request.body;
 
-	v_name_user.validaNomeUsuario(body, function(error, result){
-		
+	v_name_user.validaNomeUsuario(body, function (error, result) {
+
 	});
-	
+
 }
