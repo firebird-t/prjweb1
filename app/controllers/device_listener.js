a84146d8fe5e763b610d1de072d5392bb3b99dc2
Leonardo Everson <leonardo.everson@outlook.com>
2018-05-19 19:09:22 -0300

    include files

--- app/controllers/device_listener.js
+++ app/controllers/device_listener.js
@@ -1,3 +1,9 @@
 module.exports.gravaDadosDispositivos = function(app, request, response){
+	if(request.method == 'get'){
 
+	}
+
+	if(request.method == 'post'){
+		
+	}
 }
--- app/routes/device_listener.js
+++ app/routes/device_listener.js
@@ -1,6 +1,9 @@
 module.exports = function(app){
-	app.post('/device',function(request, response){
+	app.post('/device/post',function(request, response){
 		app.controllers.device_listener.gravaDadosDispositivos(request, response);
 	})
 
+	app.get('/device/get',function(request, response){
+		app.controllers.device_listener.gravaDadosDispositivos(request, response);
+	})
 }
